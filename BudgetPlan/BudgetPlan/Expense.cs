62232816adcea0abad9717e23c199f73886e0c46
SyurenChetty <104988398+SyurenChetty@users.noreply.github.com>
2022-05-13 19:16:51 +0200

    Add files via upload

---
+++ BudgetPlan/BudgetPlan/Expense.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlan
+{
+    abstract class Expense
+    {
+        public string Name { get; set; }
+        public float Costs { get; set; }
+
+        public Expense(string name, float costs)
+        {
+            //constructors
+            Name = name;
+            Costs = costs;
+        }
+        public override string ToString()
+        {
+            return $"{ Name}:{ Costs:C}";
+        }
+
+    }
+}
